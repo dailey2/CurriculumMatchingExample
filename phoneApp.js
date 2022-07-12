284bb8d25b267a93458357c35ba882f4e02bd0ac
dailey2 <dailey2@kenyon.edu>
2022-07-12 16:07:06 -0400

    Update phoneApp.js

--- phoneApp.js
+++ phoneApp.js
@@ -9,25 +9,6 @@ $(document).ready(function () {
     $("#clear").click(clearResults);
 });
 
-changeOperation(operation);
-
-function changeOperation(operation){
-    if(operation=="Add Entry"){
-	console.log("Add Entry");
-	$('#addmessage').empty();
-	$('.inputdata').show();
-	$('.searchbox').hide();
-	$('.results').hide();
-	$('.editdata').hide();}
-    else{
-	console.log("not add entry");
-	$('.editdata').hide();
-	$('.inputdata').hide();
-	$('.results').show();
-	$('.searchbox').show();
-    }
-}
-
 // Build output table from comma delimited list
 function buildTable(list) {
     var a = list.split(",");
@@ -49,49 +30,6 @@ function buildTable(list) {
     }
 }
 
-function processEdit(){
-    console.log("edit data");
-    $('#searchresults').empty();
-    $('.editdata').show();
-    $("#edit-btn").click(editEntry);
-    console.log("Edit Record: " + $(this).attr('ID'));
-    var row=$(this).parents("tr");
-    console.log("Subject: "+ $(row).find('.subject').text()+":"+$(row).find('.type').text());
-    editid=$(this).attr('ID');
-
-    $('#editsubject').val( $(row).find('.subject').text());
-    $('#editschoolname').val( $(row).find('.schoolName').text());
-    $('#editcoursename').val( $(row).find('.courseName').text());
-    $('#editcredits').text( $(row).find('.credits').text());
-}
-
-function editDone() {
-    $('#editmessage').text($('#editsubject').val()+" "+$('#editschoolname').val()+ " SAVED");
-}
-function editEntry(){
-    console.log("Attempting to edit an entry");
-    console.log("Subject:" + $('#editsubject').val() + "ID:" + editid);
-    $('#searchresults').empty();
-    $.ajax({
-	url: '/cgi-bin/dailey2_phoneAppComplete.cgi?editid='+editid +'&editsubject='+$('#editsubject').val()+'&editschoolname='+$('#editschoolname').val()+'&editcoursename='+$('#editcoursename').val()+'&editcredits='+$('#editcredits').text()+'&operation=edit',
-	dataType: 'text',
-	success: editDone(),
-	error: function(){alert("Error: Something went wrong");}
-    });
-}
-
-
-function processDelete(){
-    console.log("Attempting to delete an entry");
-    $('#searchresults').empty();
-    var id=$(this).attr('ID');
-    $.ajax({
-	url: '/cgi-bin/dailey2_phoneAppComplete.cgi?deleteid='+$(this).attr('ID')+'&operation=delete',
-	dataType: 'text',
-	success: function(){alert("Deleted Record: " +id );},
-	error: function(){alert("Error: Something went wrong");}
-    });
-}
 function processResults(results) {
     console.log("Results:"+results);
     $('#searchresults').empty();
@@ -111,28 +49,6 @@ function getMatches(){
 	url: '/cgi-bin/dailey2_phoneAppComplete.cgi?find='+$('#search').val()+'&operation='+operation,
 	dataType: 'text',
 	success: processResults,
-	error: function(){alert("Error: Something went wrong");}
-    });
-}
-
-function processAdd(results) {
-    $('#addmessage').empty();
-    console.log("Add:",results);
-    $('#addmessage').text($('#addsubject').val()+" "+$('#addschoolname').val()+ " ADDED");
-    $('#addsubject').val('');
-    $('#addschoolname').val('');
-    $('#addcoursename').val('');
-
-}
-
-function addEntry(){
-    console.log("Attempting to add an entry");
-    console.log("Subject:" + $('#addsubject').val());
-    $('#searchresults').empty();
-    $.ajax({
-	url: '/cgi-bin/dailey2_phoneAppComplete.cgi?asubject='+$('#addsubject').val()+'&aschoolname='+$('#addschoolname').val()+'&acoursename='+$('#coursename').val()+'&acredits='+$('#addcredits').text()+'&operation='+operation,
-	dataType: 'text',
-	success: processAdd,
 	error: function(){alert("Error: Something went wrong");}
     });
 }
